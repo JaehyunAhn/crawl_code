d0b7b9c7a8d239e3e8f277e28307b327cf5e0bb2
Jaehyun Ahn <sogosonnet@gmail.com>
2017-10-09 11:53:26 +0900

    added 정

--- convert_html_into_pdf.py
+++ convert_html_into_pdf.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 import html_edit as html_edit
 import lib as lib
+
 __desc__ = '원래 여기다가 구성하려고 했는데 func에서 테스트하느라 구현 다 해서 의미 없이 쓰고 있는 함수. 누추하지만 들어오쇼.'
 __author__ = 'sogo'
 cfg_dict = lib.read_config()
